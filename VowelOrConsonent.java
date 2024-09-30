bdda6b5cc289bae9e79493a957490266c8f8b86f
Saradha2001 <saradhasundharesan@gmail.com>
2024-09-30 17:08:30 +0530

    added VowelOrConsonant

--- VowelOrConsonent.java
+++ VowelOrConsonent.java
@@ -1,18 +1,26 @@
+//check the alphabet is Vowels or Consonant?
 import java.util.Scanner;
-class VowelOrConsonent{
+class VowelOrConsonent
+{
     public static void main(String[]args)
     {
         Scanner s=new Scanner(System.in);
-        char alphabet=s.nextChar();
+        System.out.println("Enter the word:");
+        char alphabet=s.next().charAt(0);
         if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
         {
             System.out.println("This alphabet is Vowel");
         }
         else
         {
-            System.out.println("This alphabet is Consonent");
+            System.out.println("This alphabet is Consonant");
 
         }
 
     }
 }
+/*OUTPUTS:
+Enter the word:
+icecream
+This alphabet is Vowel
+*/
