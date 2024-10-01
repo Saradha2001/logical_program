b03168231c50741b9510f139f991b089662d35f4
Saradha2001 <saradhasundharesan@gmail.com>
2024-10-01 19:40:13 +0530

    added FactorsOfNumber

--- FactorsOfNumber.java
+++ FactorsOfNumber.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
-class MultiplicationTable{
+class FactorsOfNumber{
     public static void main(String[]args)
     {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter n:");
         int n=s.nextInt();
-        for(int i=1;i<=10;i++)
+        for(int i=1;i<=n/2;i++)
         {
-            System.out.println(i+"*"+n+"="+ i*n);
+            if (n%i==0)
+            {
+                System.out.println(i);
+            }
         }
+        
+        
     }
 }
-
-/*
-OUTPUT:
+/* 
+OUTPUTS:
 Enter n:
+20
+1
+2
+4
 5
-1*5=5
-2*5=10
-3*5=15
-4*5=20
-5*5=25
-6*5=30
-7*5=35
-8*5=40
-9*5=45
-10*5=50
+10
 */
