fd03cedb9e05d551b1e2817af561bb3b4194abb5
Saradha2001 <saradhasundharesan@gmail.com>
2024-10-01 16:51:36 +0530

    addes CelsiusToFahrebheit

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -1,15 +1,27 @@
-import jav.util.Scanner;
+/*Convert Celsius to Fahrenheit?
+
+Sample Inputs:
+celsius=40
+
+Process:
+Fahrenheit=celsius*(9/5)+32
+
+Sample Outputs:
+Fahrenheit=72.0
+*/
+import java.util.Scanner;
 class CelsiusToFahrenheit{
     public static void main(String[]args)
     {
         Scanner s=new Scanner(System.in);
-        int celsius=
-        
-
-
-
-
-
-
+        System.out.println("Enter celsius:");
+        float celsius=s.nextFloat();
+        float fahrenheit=celsius*(9/5)+32;
+        System.out.println(fahrenheit);
     }
 }
+/*OUTPUTS:
+Enter celsius:
+40
+72.0
+*/
